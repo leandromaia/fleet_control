656ff0a80c7030fb9f0faf751d5198b03d6ebb16
Leadro Maia <leandroc@inatel.br>
2016-06-28 22:04:10 -0300

    Inclusão de string filter no filtro criado

--- resources/templatetags/resources_extras.py
+++ resources/templatetags/resources_extras.py
@@ -1,7 +1,9 @@
+from django.template.defaultfilters import stringfilter
 from django import template
 
 register = template.Library()
 
 @register.filter(name='replace')
+@stringfilter
 def replace(value, arg):
     return value.replace(arg, '')
