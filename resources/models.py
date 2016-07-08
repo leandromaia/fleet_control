47663934f5699b7e5a40bfd2ce9b559f6b227878
Leadro Maia <leandroc@inatel.br>
2016-07-07 21:31:57 -0300

    Models -> exercício

--- resources/models.py
+++ resources/models.py
@@ -5,3 +5,6 @@ class Vehicle(models.Model):
 
     name = models.CharField(max_length=30)
     description = models.TextField(null=True, blank=True)
+    license_plate = models.CharField(max_length=7)
+    manufacture_year = models.DateField()
+    is_active = models.BooleanField(default=True)
