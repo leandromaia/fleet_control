355562fa7b10a62e98ad0721d07912e7e5fa1758
Leadro Maia <leandroc@inatel.br>
2016-07-08 18:55:03 -0300

    Model Relation -> Correção para inclusão do modelo Manufacturer

--- resources/models.py
+++ resources/models.py
@@ -6,13 +6,18 @@ class ManagerControl(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True)
     name = models.CharField(max_length=30)
 
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+
 class Vehicle(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(null=True, blank=True)
     license_plate = models.CharField(max_length=7)
     manufacture_year = models.DateField()
     is_active = models.BooleanField(default=True)
-    usecontrols = models.ManyToManyField(Driver, through='UseControl')
+    usecontrols = models.ManyToManyField('Driver', through='UseControl')
 
 
 class Driver(models.Model):
