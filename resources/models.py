40b91b83cb9171d1c943ecd8f80d6d9ef298ac25
Leadro Maia <leandroc@inatel.br>
2016-07-08 15:10:22 -0300

    Model Relation - many to many

--- resources/models.py
+++ resources/models.py
@@ -2,9 +2,24 @@ from django.db import models
 
 
 class Vehicle(models.Model):
-
     name = models.CharField(max_length=30)
     description = models.TextField(null=True, blank=True)
     license_plate = models.CharField(max_length=7)
     manufacture_year = models.DateField()
     is_active = models.BooleanField(default=True)
+    usecontrols = models.ManyToManyField(Driver, through='UseControl')
+
+
+class Driver(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class UseControl(models.Model):
+    driver = models.ForeignKey(Driver)
+    vehicle = models.ForeignKey(Vehicle)
+    date_started = models.DateTimeField(auto_now_add=True)
+    date_ended = models.DateTimeField(blank=True, null=True)
+
+
+class Manager(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True)
