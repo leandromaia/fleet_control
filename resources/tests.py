59697a5a7a16d265fc0e41be61d5c0015efcc405
Leadro Maia <leandroc@inatel.br>
2016-07-12 09:09:10 -0300

    Tests -> primeiro teste com TestClient

--- resources/tests.py
+++ resources/tests.py
@@ -14,3 +14,9 @@ class VehicleTest(TestCase):
     def test_models(self):
         self.assertTrue(self.vehicle.is_active)
         self.assertEqual(self.vehicle.usecontrols.count(), 0)
+
+
+class VehicleViewTests(TestCase):
+    def test_vehicle_list(self):
+        response = self.client.get('/resources/vehicle_list/')
+        self.assertEqual(response.status_code, 200)
