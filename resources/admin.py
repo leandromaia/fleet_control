5c2d1bb10575d37c15bbdaf9c7649d317b83a90c
Leadro Maia <leandroc@inatel.br>
2016-07-13 09:02:32 -0300

    Admin -> criação de herança de ModelAdmin

--- resources/admin.py
+++ resources/admin.py
@@ -3,8 +3,23 @@ from django.contrib import admin
 from .models import ManagerControl, Manufacturer, \
                             Vehicle, Driver, UseControl
 
-admin.site.register(ManagerControl)
-admin.site.register(Manufacturer)
-admin.site.register(Vehicle)
-admin.site.register(Driver)
-admin.site.register(UseControl)
+
+@admin.register(ManagerControl)
+class ManagerControlAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Manufacturer)
+class ManufacturerControlAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Vehicle)
+class VehicleControlAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Driver)
+class DriverControlAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UseControl)
+class UseControlControlAdmin(admin.ModelAdmin):
+    pass
