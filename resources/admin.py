be03bbecae035a39439c46d2c7f12530c6615253
Leadro Maia <leandroc@inatel.br>
2016-07-13 09:45:05 -0300

    Admin -> customizando a listagem de Vehicle

--- resources/admin.py
+++ resources/admin.py
@@ -1,25 +1,26 @@
 from django.contrib import admin
-
 from .models import ManagerControl, Manufacturer, \
                             Vehicle, Driver, UseControl
-
 
 @admin.register(ManagerControl)
 class ManagerControlAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Manufacturer)
-class ManufacturerControlAdmin(admin.ModelAdmin):
+class ManufacturerAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Vehicle)
-class VehicleControlAdmin(admin.ModelAdmin):
-    pass
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'license_plate','manufacture_year', 'is_active')
+    list_display_links = ('name', 'license_plate')
+    list_per_page = 2
+    ordering = ('name', )
 
 @admin.register(Driver)
-class DriverControlAdmin(admin.ModelAdmin):
+class DriverAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(UseControl)
-class UseControlControlAdmin(admin.ModelAdmin):
+class UseControlAdmin(admin.ModelAdmin):
     pass
