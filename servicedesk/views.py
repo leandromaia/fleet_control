516d5591fd49cb68730851f4d47310bafebd031e
Leadro Maia <leandroc@inatel.br>
2016-07-14 17:21:13 -0300

    Forms -> inserção de labels

--- servicedesk/views.py
+++ servicedesk/views.py
@@ -10,18 +10,11 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            sender = form.cleaned_data['sender']
-            cc_myself = form.cleaned_data['cc_myself']
-
-            recipients = ['leandroc@inatel.br']
-            if cc_myself:
-                recipients.append(sender)
-
-            send_mail(subject, message, sender, recipients)
+            # Processa os dados no form.cleaned_data
+            # ...
             return HttpResponseRedirect('/servicedesk/thanks')
     else:
         form = ContactForm()
 
-    return render_to_response('contact.html', {'form': form,},  RequestContext(request))
+    return render_to_response('contact.html', {'form': form,},  \
+                                            RequestContext(request))
