9e070295d4f15fbab8e7613066177e7c2d89f1c5
Leadro Maia <leandroc@inatel.br>
2016-07-14 20:55:48 -0300

    ModelForms -> exemplo inicial com Vehicle

--- servicedesk/forms.py
+++ servicedesk/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100, label='Assunto')
+    subject = forms.CharField(label='Assunto')
     message = forms.CharField(widget=forms.Textarea, label='Mensagem')
     sender = forms.EmailField(label='Email')
     cc_myself = forms.BooleanField(required=False, label='Me Copiar')
