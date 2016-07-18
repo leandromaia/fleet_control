0be638c784c0ae8024fb0a6b2d8215309a899f3f
Leadro Maia <leandroc@inatel.br>
2016-07-18 11:46:07 -0300

    Tests -> inclusão de testes assertRaises

--- resources/tests/test_vehicle.py
+++ resources/tests/test_vehicle.py
@@ -1,36 +1,75 @@
 from datetime import date
+
 from django.test import TestCase
-from resources.models import Vehicle
+from django.core.urlresolvers import reverse
+
+from resources.models import Vehicle, Manufacturer
+
+
+class VehicleTests(TestCase):
+
+    def setUp(self):
+        manufacturer = Manufacturer(name='Fiat Company')
+        manufacturer.save()
+        year_manufacture = date(2007, 1, 1)
+        self.vehicle = Vehicle(
+            name='Uno', license_plate='WRF8900', \
+            manufacture_year=year_manufacture, manufacturer=manufacturer
+        )
+        self.vehicle.save()
+
+    def test_vehicle_is_active_must_be_true(self):
+        self.assertTrue(self.vehicle.is_active)
 
 
 class VehicleViewTests(TestCase):
+    fixtures = ['vehicle_fixture']
+    def setUp(self):
+        self.manufacturer = Manufacturer.objects.create(name='Fiat')
+
+    def test_fixture_value(self):
+        vehicle = Vehicle.objects.get(pk=1)
+        self.assertEqual(vehicle.name, 'Uno')
+        self.assertEqual(vehicle.manufacturer.name, 'Fiat')
+
 
     def test_must_create_vehicle(self):
         data = {
-            "name": "Fiat Palio",
-            "description": "Modelo popular, cor branca",
-            "license_plate": "GMF5689",
-            "manufacture_year": '2008-01-01'
+            'name': 'Uno Fire',
+            'description': 'Carro bão',
+            'license_plate': 'GGM8976',
+            'manufacture_year': '2007-01-01',
+            'manufacturer': self.manufacturer.id
         }
-        response = self.client.post('/resources/vehicle/add/', data)
-        #Check status code for redirect
+        url = reverse('vehicle_add')
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, 302)
         veh_saved = Vehicle.objects.all().first()
-        self.assertEqual(veh_saved.id, 1)
-        self.assertEqual(veh_saved.name, 'Fiat Palio')
+        self.assertEqual(veh_saved.id, 2)
+        self.assertEqual(veh_saved.name, 'Uno Fire')
 
-    def test_vehicle_list(self):
-        response = self.client.get('/resources/vehicle_list/')
-        self.assertEqual(response.status_code, 200)
+class VehicleUpdateViewTests(TestCase):
+    fixtures= ['vehicle_fixture']
+
+    def test_must_update_vehicle_by_view(self):
+        data = {
+            'name': 'Uno Fire',
+            'description': 'Carro bao',
+            'license_plate': 'GGE0098',
+            'manufacture_year': '2007-01-01',
+            'manufacturer': 1
+        }
+        url = reverse('vehicle_update', kwargs={'pk': 1})
+        response = self.client.post(url, data)
+        vehicle = Vehicle.objects.get(pk=1)
+        self.assertEqual(vehicle.name, 'Uno Fire')
 
 
-class VehicleTest(TestCase):
+class VehicleDeleteViewTests(TestCase):
+    fixtures= ['vehicle_fixture']
 
-    def setUp(self):
-        date_manufacture = date(2010, 1, 1)
-        self.vehicle = Vehicle.objects.create(name='Uno', \
-                license_plate='GMF7860', manufacture_year=date_manufacture)
-
-    def test_models(self):
-        self.assertTrue(self.vehicle.is_active)
-        self.assertEqual(self.vehicle.usecontrols.count(), 0)
+    def test_must_delete_vehicle_by_view(self):
+        url = reverse('vehicle_delete', kwargs={'pk': 1})
+        response = self.client.post(url)
+        vehicle_list = Vehicle.objects.all()
+        self.assertRaises(Vehicle.DoesNotExist, Vehicle.objects.get, pk=1)
